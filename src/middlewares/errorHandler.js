bc50c9c66b515a8ffcd0438ec3a60c203c24abc4
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-11-08 15:35:12 +0100

    update error new

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,8 @@
-export function errorHandler(err, req, res, next){
-res.status(500).json({
-status: 500,
-message: "Something went wrong",
-data: err.message,
-});
+export function errorHandler(err, req, res, next) {
+    const status = err.status || 500; 
+    res.status(status).json({
+        status,
+        message: err.message || "Something went wrong",
+        data: null,
+    });
 }
